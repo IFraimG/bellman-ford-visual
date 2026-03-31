0518d491168e1843e7edf5426bea5c6cb7968d17
IFraimG <akulagin@mts.ru>
2026-03-31 05:53:13 +0300

    fix: обновление алгоритма

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -1,17 +1,25 @@
+type GraphEdge = [number, number, number]
+
 export const execute = (
-  graph: number[][],
+  graph: GraphEdge[],
   verCount: number = 0,
   verStart: number = 0,
   verEnd: number = 0,
 ): number => {
-  const dist: number[] = Array.from({ length: verCount! }, () => Infinity)
+  if (
+    verCount <= 0 ||
+    graph.length == 0 ||
+    graph.length != graph.filter((item) => item.length == 3).length
+  ) {
+    return 0
+  }
+
+  const dist: number[] = Array.from({ length: verCount }, () => Infinity)
   dist[verStart] = 0
 
   for (let i = 0; i < verCount - 1; i++) {
     for (const item of graph) {
-      const u: number = item[0] ?? 0
-      const v: number = item[1] ?? 0
-      const w: number = item[2] ?? 0
+      const [u, v, w]: GraphEdge = item
       if (dist[u] !== Infinity && dist[u]! + w < dist[v]!) {
         dist[v] = dist[u]! + w
       }
@@ -19,14 +27,11 @@ export const execute = (
   }
 
   for (const item of graph) {
-    const u: number = item[0] ?? 0
-    const v: number = item[1] ?? 0
-    const w: number = item[2] ?? 0
-    if (dist[u] != Infinity && dist[u]! + w < dist[v]!) {
-      console.log('???')
+    const [u, v, w]: GraphEdge = item
+    if (dist[u] !== Infinity && dist[u]! + w < dist[v]!) {
+      throw new Error('Graph contains a negative weight cycle')
     }
   }
 
-  dist.map((item, index) => console.log(index, item))
-  return dist[verEnd]
+  return dist[verEnd]!
 }
